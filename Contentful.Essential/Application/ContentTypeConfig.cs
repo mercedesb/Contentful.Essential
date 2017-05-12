5ce30ee639de22ae90e73f44566ff882397e09ee
mercedesb <mercedesbernard@live.com>
2017-05-11 21:48:50 -0500

    refactored content type config to get registered types (instead of only scanning defined assembly)

--- Contentful.Essential/Application/ContentTypeConfig.cs
+++ Contentful.Essential/Application/ContentTypeConfig.cs
@@ -1,16 +1,23 @@
 ﻿using Contentful.CodeFirst;
 using Contentful.Essential.Configuration;
+using Contentful.Essential.Models;
+using Microsoft.Practices.ServiceLocation;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Contentful.Essential.Application
 {
     public class ContentTypeConfig
     {
+        // TODO: should this go in the sample site?
         public static async Task RegisterContentTypes()
         {
-            // TODO: control which assembly is scanned
-            await ContentTypeBuilder.CreateContentTypesFromAssembly(typeof(ContentTypeConfig).Assembly.FullName, GetConfig());
-
+            IEnumerable<IContentType> registeredContentTypes = ServiceLocator.Current.GetAllInstances<IContentType>();
+            var types = registeredContentTypes.Select(ct => ct.GetType()).Where(c => c.GetTypeInfo().IsClass && c.GetTypeInfo().GetCustomAttribute<ContentTypeAttribute>() != null);
+            var contentTypesToCreate = ContentTypeBuilder.InitializeContentTypes(types);
+            var createdContentTypes = await ContentTypeBuilder.CreateContentTypes(contentTypesToCreate, GetConfig(), ContentManagement.Instance);
         }
 
         private static ContentfulCodeFirstConfiguration GetConfig()
